3efba1b63a4b8676dd68a6f1d7453aa40245025e
Ted Irland <irlandth@gmail.com>
2021-06-02 09:00:35 -0500

    Created 2 additional components in addition to the App.vue. Block and results. Wired block component up to the app.vue and displayed to screen

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+<h1> Ninja Reaction Timer </h1>
+<button @click="start" :disabled="isPlaying"> Play</button>
+<Block v-if="isPlaying" :delay="delay" />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Block},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying=true;
+    }
   }
 }
 </script>
@@ -20,7 +34,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
