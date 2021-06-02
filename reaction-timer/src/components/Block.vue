7d54c5f93496284087f810b0791a373284ccfcbf
Ted Irland <irlandth@gmail.com>
2021-06-02 09:14:27 -0500

    Implemented some lifecycle hooks to understand how they work. For this project we'll be using the mounted lifecycle hook but also got some context into updated and unmounted

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -1,12 +1,22 @@
 <template>
-<div class="block">Click Me</div>
+<div class="block" v-if="showBlock">Click Me</div>
 
 </template>
 
 <script>
 export default {
-  props: ['delay']
-
+  props: ['delay'],
+  data() {
+    return {
+     showBlock: false
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock=true
+      console.log(this.delay)
+    }, this.delay)
+  }
 }
 </script>
 
