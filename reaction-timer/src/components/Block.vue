c2d481bcdfbb9da2f859dca7d16c3da5b9e97163
Ted Irland <irlandth@gmail.com>
2021-06-02 09:21:05 -0500

    Created a reaction timer functionality inside the Block.vue component. Used setInterval to start a timer, assigned it to the timer property and created another method using clearInterval that is called when the block component is clicked.

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="block" v-if="showBlock">Click Me</div>
+<div class="block" v-if="showBlock" @click="stopTimer">Click Me</div>
 
 </template>
 
@@ -8,15 +8,29 @@ export default {
   props: ['delay'],
   data() {
     return {
-     showBlock: false
+     showBlock: false,
+     timer: null,
+     reactionTime: 0
     }
   },
   mounted() {
     setTimeout(() => {
       this.showBlock=true
-      console.log(this.delay)
+     this.startTimer()
+
     }, this.delay)
-  }
+  },
+  methods: {
+    startTimer() {
+      this.timer= setInterval(() => {
+        this.reactionTime += 10
+      }, 10)
+    },
+    stopTimer() {
+      clearInterval(this.timer)
+      console.log(this.reactionTime)
+    }
+      }
 }
 </script>
 
